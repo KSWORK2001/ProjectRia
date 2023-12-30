b6277d67b1998ca3dd29fadb55b93301512accc6
Karan Shrivastava <work.karan2001@gmail.com>
2023-12-30 05:54:25 -0500

    Spotify Functionality added

--- ProjectRia.py
+++ ProjectRia.py
@@ -2,13 +2,31 @@ import speech_recognition as sr
 from gtts import gTTS
 import pygame
 import io
+import subprocess
 
-def recognize_speech():
+"""def recognize_wake_word():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening for wake word...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        wake_word = recognizer.recognize_google(audio).lower()
+        return "hey ria" in wake_word
+    except sr.UnknownValueError:
+        return False
+    except sr.RequestError as e:
+        print(f"Could not request results; {e}")
+        return False"""
+
+def recognize_speech(timeout=3):
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say something:")
         recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source, timeout=5)
+        audio = recognizer.listen(source, timeout=timeout)
+
     try:
         text = recognizer.recognize_google(audio)
         return text
@@ -20,14 +38,23 @@ def recognize_speech():
         return None
 
 def generate_response(user_input):
-    responses = {
-        "hey ria": "Hey there, how can I help you today?",
-        "how are you": "I'm doing well, thank you!",
-        "what's your name": "I'm Ria! I am a virtual assistant desgned to help you tackle school!",
-        "bye": "Goodbye! Have a great day.",
-    }
+    if "open spotify" in user_input.lower():
+        open_spotify()
+        return "Opening Spotify..."
+    elif "how are you" in user_input.lower():
+        return "I'm doing well, thank you!"
+    elif "what's your name" in user_input.lower():
+        return "I'm a virtual assistant. You can call me Ria."
+    elif "bye" in user_input.lower():
+        return "Goodbye! Have a great day."
+    else:
+        return "I didn't understand that."
 
-    return responses.get(user_input.lower(), "I didn't understand that.")
+def open_spotify():
+    try:
+        subprocess.Popen(["C:\\Users\\karan\\AppData\\Roaming\\Spotify\\Spotify.exe"])
+    except Exception as e:
+        print(f"Error opening Spotify: {e}")
 
 def speak(text):
     tts = gTTS(text=text, lang='en')
@@ -45,12 +72,14 @@ def speak(text):
         pygame.time.Clock().tick(10)
 
 if __name__ == "__main__":
-    speak("Hello! I'm Ria. How can I help you today?")
+    speak("Hey there! I'm Ria... How can I help you today?")
     
     while True:
-        user_input = recognize_speech()
-        if user_input:
-            print("You said:", user_input)
-            response = generate_response(user_input)
-            print("Assistant:", response)
-            speak(response)
+        #if recognize_wake_word():                                              #Work on Recognizing Wake Word
+        if True:
+            user_input = recognize_speech(timeout=5)
+            if user_input:
+                print("You said:", user_input)
+                response = generate_response(user_input)
+                print("Ria:", response)
+                speak(response)
