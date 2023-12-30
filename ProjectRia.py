96d8cd3d1b725e3c824cbf93aea5ee3624aff7f9
Karan Shrivastava <work.karan2001@gmail.com>
2023-12-30 17:15:17 -0500

    Weather Functionality Added, Roblox Added, Speed Optimized. Audio Pickup Optimzed

--- ProjectRia.py
+++ ProjectRia.py
@@ -4,12 +4,15 @@ from gtts import gTTS
 import pygame
 import io
 import os
+import geocoder
+import requests
 
-def recognize_speech(timeout=3):
+def recognize_speech(timeout=5, custom_energy_threshold=4000):
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say something:")
         recognizer.adjust_for_ambient_noise(source)
+        recognizer.energy_threshold = custom_energy_threshold
         audio = recognizer.listen(source, timeout=timeout)
 
     try:
@@ -22,19 +25,29 @@ def recognize_speech(timeout=3):
         print(f"Could not request results; {0}".format(e))
         return None
 
-def generate_response(user_input):
+def generate_response(user_input, api_key):
     if any(phrase in user_input for phrase in ["goodbye", "bye", "see you"]):
-        return "See Ya! Have a great day."
-    elif "open spotify" in user_input.lower():
+        return "Seeya! Have a great day."
+    elif "spotify" in user_input.lower():
         open_application("Spotify")
         return "Opening Spotify..."
-    elif "open chrome" in user_input.lower():
+    elif "chrome" in user_input.lower():
         open_application("Chrome")
         return "Opening Chrome..."
+    elif "roblox" in user_input.lower():
+        open_application("Roblox")
+        return "Opening Roblox..."
     elif "how are you" in user_input.lower():
         return "I'm doing well, thank you!"
     elif "what's your name" in user_input.lower():
-        return "I'm a virtual assistant. You can call me Ria."
+        return "I am Ria. I am created by my legendary brother."
+    elif "weather" in user_input.lower():
+        user_city, user_country, user_location = get_user_location()
+        if user_city and user_country and user_location:
+            weather_info = get_weather(user_location[0], user_location[1], api_key)
+            return f"Current weather in {user_city}, {user_country}: {weather_info}"
+        else:
+            return "Could not determine user location."
     elif "bye" in user_input.lower():
         return "Goodbye! Have a great day."
     else:
@@ -46,6 +59,8 @@ def open_application(application_name):
             os.startfile("C:\\Users\\karan\\AppData\\Roaming\\Spotify\\Spotify.exe")
         elif application_name == "Chrome":
             os.startfile("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
+        elif application_name == "Roblox":
+            os.startfile("C:\\Users\\karan\\AppData\\Local\\Roblox\\Versions\\version-510663c9d33e4fd8\\RobloxPlayerBeta.exe")
     except Exception as e:
         print(f"Error opening {application_name}: {e}")
 
@@ -64,22 +79,54 @@ def speak(text, lang='en'):
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
+def get_user_location():
+    try:
+        location = geocoder.ip('me')
+        return location.city, location.country, location.latlng
+    except Exception as e:
+        return None, None, None
+
+def get_weather(lat, lon, api_key):
+    base_url = "https://api.openweathermap.org/data/3.0/onecall"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "exclude": "minutely,hourly",
+        "appid": api_key,
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            # Process and extract relevant weather information from 'data'
+            # Note: 'data' may contain various weather details for the specified location
+            # Extract the information you need based on your requirements.
+            return "Weather details here"
+        else:
+            return f"Error: {data['message']}"
+    except Exception as e:
+        return f"Error: {e}"
+
 if __name__ == "__main__":
+    # Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual OpenWeatherMap API key
+    api_key = 'ALREADY PUT THE KEY'
+
+
     speak("Hey there! I'm Ria... How can I help you today?")
     
     exit_program = False
     
     while not exit_program:
-        user_input = recognize_speech(timeout=2)
+        user_input = recognize_speech(timeout=5)
         if user_input:
             print("You said:", user_input)
-            response = generate_response(user_input)
+            response = generate_response(user_input, api_key)
             print("Ria:", response)
             speak(response)
 
             if any(phrase in user_input for phrase in ["goodbye", "bye", "see you"]):
                 exit_program = True
-            elif "open spotify" in user_input.lower():
+            elif "spotify" in user_input.lower():
                 open_application("Spotify")
-            elif "open chrome" in user_input.lower():
-                open_application("Chrome")
